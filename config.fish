5dcad6904ec9e0781529fdfcb6f90594fcc3fd8a
Sarthak Tomar <sarthaktomar2143@gmail.com>
2024-12-25 15:13:31 +0530

    nthg much.

--- config.fish
+++ config.fish
@@ -2,3 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     source ~/.bash_aliases
 end
+
+function fish_greeting # disables the stupid welcome message
+end
